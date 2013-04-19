2342845a38326e23f72a46e311862aae95f8f793
William Clifford <wobh@yahoo.com>
2013-04-19 00:19:40 -0700

    NEW options for public and views folders

--- singer.rb
+++ singer.rb
@@ -8,11 +8,18 @@ require 'fileutils'
 class Singer
   attr_reader :project_name, :project_path
   attr_reader :gemfile_text, :appfile_text
+  attr_reader :build_opts
+
+  def set_opts(args)
+    @build_opts = {}
+    @build_opts.update(args)
+  end
   
   def initialize (args)
-    @project_name = args[:project_name]
+    set_opts(args)
+    @project_name = build_opts[:project_name]
     
-    if args[:with_gemfile]
+    if build_opts[:with_gemfile]
       @gemfile_text = <<-EOF
 # #{project_name} Gems
 
@@ -23,7 +30,7 @@ gem 'sinatra-contrib', :github => "sinatra/sinatra-contrib"
 EOF
     end
 
-    if args[:with_appfile]
+    if build_opts[:with_appfile]
       @appfile_text = <<-EOF
 # #{project_name} Application
 
@@ -50,7 +57,14 @@ EOF
 
   def make_folder_public
     # Make a public folder
-    return make_path_in_project('public')
+    pubdir = 'public'
+    make_path_in_project(pubdir)
+    if build_opts[:fill_public]
+      ['stylesheets', 'javascript', 'images'].each do |dirname|
+        make_path_in_project(File.join(pubdir, dirname))
+      end
+    end
+    return File.join(project_path, pubdir)
   end
 
   def make_folder_views
@@ -71,8 +85,21 @@ EOF
   def create()
     # Create project
     make_folder_project()
-    make_folder_public()
-    make_folder_views()
+    if build_opts[:with_public]
+      make_folder_public()
+    end
+
+    if build_opts[:with_views]
+      make_folder_views()
+    end
+
+    if build_opts[:with_appfile]
+      make_appfile()
+    end
+
+    if build_opts[:with_gemfile]
+      make_gemfile()
+    end
   end
 
   def remove()
@@ -88,25 +115,46 @@ if __FILE__ == $PROGRAM_NAME
 
   options = {}
 
-  OptionParser.new do |opts|
+  optparse = OptionParser.new do |opts|
     opts.banner = "Usage: singer.rb [OPTIONS] PROJECT_NAME"
 
+    opts.on("-g", "--gemfile", "Build a Gemfile in project") do
+      options[:with_gemfile] = true
+    end
+
+    opts.on("-a", "--appfile", "Build an app file in project") do
+      options[:with_appfile] = true
+    end
+
+    opts.on("--public", "Build a public folder in project") do
+      options[:with_public] = true
+    end
+
+    opts.on("--public-fill", "Build a public folder with some standard sub-folders") do
+      options[:with_public] = true
+      options[:fill_public] = true
+    end
+
+    opts.on("--views", "Build a views folder in project") do
+      options[:with_views] = true
+    end
 
     opts.on_tail("-h", "--help", "Show this message") do
       puts opts
       exit
     end
     
-    if ARGV == []
-      puts opts
-      exit
-    else
-      options[:project_name] = ARGV[0]
-    end
-
   end.parse!
 
-  
+  # Parse first remaining argument in ARGV
+  if ARGV.empty?
+    puts optparse
+    exit
+  else
+    options[:project_name] = ARGV[0]
+  end
+
+
   singer = Singer.new(options)
   singer.create()
   
